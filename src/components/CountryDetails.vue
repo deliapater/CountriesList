6ba1ff143bc38a109d21dd201ea9c78156732cb9
Dev <dev@Delias-MacBook-Air.local>
2019-02-22 10:50:03 +0000

    advance extensions updated

--- src/components/CountryDetails.vue
+++ src/components/CountryDetails.vue
@@ -1,8 +1,13 @@
 <template lang="html">
-<div v-if="country">
+<div v-if="country" class="info-container">
   <h1>{{country.name}}</h1>
   <p>Capital: {{country.capital}}</p>
   <p>Population: {{country.population}}</p>
+  <img :src="country.flag" :alt="country.flag" id="flags">
+  <p>Language: </p>
+  <ul v-for="language in country.languages">
+      <li>{{language.name}}</li>
+    </ul>
 
 </div>
 
@@ -16,4 +21,15 @@ export default {
 </script>
 
 <style lang="css" scoped>
+
+#flags {
+  width:400px;height:200px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+}
+
+.info-container {
+
+}
 </style>
