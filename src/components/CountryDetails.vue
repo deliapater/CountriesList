2f78546b699defc012a259cc66d7eeceac0b1c30
Dev <dev@Delias-MacBook-Air.local>
2019-02-22 11:58:24 +0000

    style updated

--- src/components/CountryDetails.vue
+++ src/components/CountryDetails.vue
@@ -1,8 +1,8 @@
 <template lang="html">
 <div v-if="country" class="info-container">
   <h1>{{country.name}}</h1>
-  <p>Capital: {{country.capital}}</p>
   <img :src="country.flag" :alt="country.flag" id="flags">
+  <p>Capital: {{country.capital}}</p>
   <p>Population: {{country.population}}</p>
   <p>Languages: </p>
   <ul v-for="language in country.languages">
